ac717e40ad33feed985475c8330e2677cad0433c
filanor <filanor@gmail.com>
2022-01-29 23:20:01 +0300

    finish project config

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+const { off } = require("process");
+
+module.exports = {
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    babelOptions: {
+      configFile: './babel.config.json'
+    }
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true
+  },
+  extends: ['eslint:recommended', 'google'],
+  rules: {
+    'semi': 'off',
+    'comma-dangle': 'off',
+    'require-jsdoc': 'off',
+  }
+}
